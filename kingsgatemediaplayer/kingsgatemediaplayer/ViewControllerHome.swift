249f773b9accc412d4231928114586272249a12d
Jon Burney <jon@version7.co.uk>
2016-04-25 20:39:09 +0100

    Testing out adding some storyboards and background images via assets

--- kingsgatemediaplayer/kingsgatemediaplayer/ViewControllerHome.swift
+++ kingsgatemediaplayer/kingsgatemediaplayer/ViewControllerHome.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerHome: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setBackgroundImage()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setBackgroundImage() {
+        let uiImage = UIImage(named:"Background")
+        
+        self.view.backgroundColor = UIColor(patternImage: uiImage!)
+
     }
 
     override func didReceiveMemoryWarning() {
